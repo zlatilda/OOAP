4522f5871c9cf21a75ef69ee16ceb3135a556562
danylo1999 <danylo1999@gmail.com>
2021-01-24 09:46:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,13 @@ import io
 from setuptools import find_packages
 from setuptools import setup
 
-with io.open("README.rst", "rt", encoding="utf8") as f:
-    readme = f.read()
-
 setup(
     name="flaskr",
     version="1.0.0",
-    url="http://flask.pocoo.org/docs/tutorial/",
-    license="BSD",
-    maintainer="Pallets team",
-    maintainer_email="contact@palletsprojects.com",
-    description="The basic blog app built in the Flask tutorial.",
-    long_description=readme,
+    license="MIT",
+    maintainer="Danylo Mocherniuk",
+    maintainer_email="danylo1999@gmail.com",
+    description="Basic tool to play with ML models.",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
